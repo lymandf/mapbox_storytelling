9c7eb8b7a85195177a2598e3ccbbdf017f669c42
lymandf <lymandf@dukes.jmu.edu>
2025-05-06 14:47:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Color Purple Essay 2',
+    title: 'Final Project',
     subtitle: 'New Essay Title',
-    byline: 'Danny',
+    byline: 'Danny Lyman and Alex Greeves',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
